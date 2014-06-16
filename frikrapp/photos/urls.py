2172c750a2badf429466c4dc11e298446832390d
Alberto Casero <alberto@kasfactory.net>
2014-06-16 19:00:13 +0200

    He cambiado las URLs para que cada una tenga un nombre a modo de identificador.
    De esta manera, no es necesario escribir las URLs en el sistema sino que podemos usar nombres de manera que, si alguien decide cambiar la URL, no tenemos que revisar las plantillas donde aparece, sino simplemente cambiar el patrón y listo.
    
    Al referenciar las URLs por nombre, un cambio de URL en una web no es nada doloroso.
    
    ¿Mola o no?

--- frikrapp/photos/urls.py
+++ frikrapp/photos/urls.py
@@ -3,11 +3,11 @@ from photos import views
 
 # Web URLS
 urlpatterns = patterns('',
-    url(r'^$', views.HomeView.as_view()),
-    url(r'^photos/$', views.PhotoListView.as_view()),
-    url(r'^photos/(?P<pk>[0-9]+)$', views.PhotoDetailView.as_view()),
-    url(r'^login$', views.UserLoginView.as_view()),
-    url(r'^logout$', views.UserLogoutView.as_view()),
-    url(r'^profile$', views.UserProfileView.as_view()),
-    url(r'^create$', 'photos.views.create_photo'),
+    url(r'^$', views.HomeView.as_view(), name='home'),
+    url(r'^photos/$', views.PhotoListView.as_view(), name='photo_list'),
+    url(r'^photos/(?P<pk>[0-9]+)$', views.PhotoDetailView.as_view(), name='photo_detail'),
+    url(r'^login$', views.UserLoginView.as_view(), name='user_login'),
+    url(r'^logout$', views.UserLogoutView.as_view(), name='user_logout'),
+    url(r'^profile$', views.UserProfileView.as_view(), name='user_profile'),
+    url(r'^create$', 'photos.views.create_photo', name='new_photo'),
 )
