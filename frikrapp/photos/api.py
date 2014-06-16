07f7e4ed35f3e3670b87a5236cb109ec504cff1f
Alberto Casero <alberto@kasfactory.net>
2014-06-16 19:10:51 +0200

    Creo el viewset de usuarios, que no se basa en un ModelViewset. Simplemente junto las clases UserListAPI y UserDetailAPI y renombro los métodos:
    UserListAPI.get -> list
    UserListAPI.post -> create
    UserDetailAPI.get -> retrieve
    UserDetailAPI.put -> update
    UserDetailAPI.delete -> destroy

--- frikrapp/photos/api.py
+++ frikrapp/photos/api.py
@@ -23,15 +23,16 @@ WELCOME_EMAIL_SUBJECT = 'Hola {0} {1}!'
 WELCOME_EMAIL_FROM = 'root@localhost'
 DEBUG = getattr(settings, 'DEBUG', False)
 
-class UserListAPI(APIView):
 
-    def get(self, request):
+class UserAPI(viewsets.ViewSet):
+
+    def list(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
 
 
-    def post(self, request):
+    def create(self, request):
         serializer = UserSerializer(data=request.DATA) # en lugar request.POST
         if serializer.is_valid():
             new_user = serializer.save()
@@ -40,29 +41,7 @@ class UserListAPI(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-    def send_welcome_email(self, user):
-        # recogemos el asunto, direccion remitente y destino
-        subject = WELCOME_EMAIL_SUBJECT.format(user.first_name, user.last_name)
-        from_email = WELCOME_EMAIL_FROM
-        to = user.email
-
-        # Renderizamos la plantilla con el e-mail de bienvenida
-        template = loader.get_template(WELCOME_EMAIL_TEMPLATE)
-        context = Context( { 'new_user': user } )
-        html_content = template.render(context) # renderizamos la plantilla
-        text_content = strip_tags(html_content) # texto plano sin HTML
-
-        # creamos un mensaje de e-mail
-        msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
-        msg.attach_alternative(html_content, "text/html")
-        msg.send(fail_silently=(not DEBUG)) # si falla el envío de e-mail, no peta
-
-
-
-class UserDetailAPI(APIView):
-
-    def get(self, request, pk):
+    def retrieve(self, request, pk):
         user = get_object_or_404(User, pk=pk)
         if request.user.is_superuser or request.user == user:
             serializer = UserSerializer(user)
@@ -71,7 +50,7 @@ class UserDetailAPI(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
 
-    def put(self, request, pk):
+    def update(self, request, pk):
         user = get_object_or_404(User, pk=pk)
         if request.user.is_superuser or request.user == user:
             serializer = UserSerializer(user, data=request.DATA)
@@ -84,13 +63,32 @@ class UserDetailAPI(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
 
-    def delete(self, request, pk):
+    def destroy(self, request, pk):
         user = get_object_or_404(User, pk=pk)
         if request.user.is_superuser or request.user == user:
             user.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+    def send_welcome_email(self, user):
+        # recogemos el asunto, direccion remitente y destino
+        subject = WELCOME_EMAIL_SUBJECT.format(user.first_name, user.last_name)
+        from_email = WELCOME_EMAIL_FROM
+        to = user.email
+
+        # Renderizamos la plantilla con el e-mail de bienvenida
+        template = loader.get_template(WELCOME_EMAIL_TEMPLATE)
+        context = Context({'new_user': user})
+        html_content = template.render(context)  # renderizamos la plantilla
+        text_content = strip_tags(html_content)  # texto plano sin HTML
+
+        # creamos un mensaje de e-mail
+        msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send(fail_silently=(not DEBUG))  # si falla el envío de e-mail, no peta
+
 
 
 
--- frikrapp/photos/api_urls.py
+++ frikrapp/photos/api_urls.py
@@ -7,11 +7,13 @@ from rest_framework.routers import DefaultRouter
 photo_router = DefaultRouter()
 photo_router.register(r'photos', api.PhotoAPI)
 
+user_router = DefaultRouter()
+user_router.register(r'users', api.UserAPI, base_name='user') # necesita base_name porque como no es un ModelViewSet, no sabe como nombrar internamente las URL
+
 urlpatterns = patterns('',
 
     # User API URLs
-    url(r'^users/$', api.UserListAPI.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)$', api.UserDetailAPI.as_view()),
+    url(r'^', include(user_router.urls)),
 
     # Photo API URLs
     url(r'^', include(photo_router.urls)),
